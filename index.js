37638f404c7ef5055cfa6ac172a6682a96dcf3cc
jonsirrine <sir15004@byui.edu>
2018-06-18 06:27:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+const port = 8888;
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+   socket.on('disconnect', () => {
+      console.log("user diconnected");
+   });
+   socket.on('new user', (username) => {
+      console.log(username);
+      io.emit('new connection', username);
+   })
+});
+
+http.listen(port, () => {
+   console.log('listening on :' + port);
+});
