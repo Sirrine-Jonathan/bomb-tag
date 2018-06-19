c49e901d4c97ee498daf9aa0b80901247f884cd7
jonsirrine <sir15004@byui.edu>
2018-06-19 07:15:58 -0700

    trying new user format

--- index.js
+++ index.js
@@ -9,44 +9,29 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 const port = (process.env.PORT) ? process.env.PORT:8888;
-let usersOnline = [];
+let users = {};
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-   socket.emit('update users', usersOnline);
-   socket.on('disconnecting', () => {
-      let id = -1;
-      for (let i = 0; i < usersOnline.length; i++){
-         if (usersOnline[i].id == socket.id){
-            id = i;
-            break;
-         }
-      };
-      if (id > 0){
-         console.log(usersOnline[id].name + " left");
-         usersOnline.splice(id, 1);
-      } else {
-         console.log("unknown error");
-      }
-   });
+   io.emit('updateusers', users);
 
-   socket.on('disconnect', () => {
-      socket.broadcast.emit('update users', usersOnline);
-   });
 
    socket.on('new user', (user) => {
-      console.log(user.name + " logged on");
-      usersOnline.push(user);
-      console.log("New User Added, Showing List: ");
-      for (let i = 0; i < usersOnline.length; i++){
-         console.log(user.id + ", " + user.name);
-      }
-      console.log("");
-      io.emit('update users', usersOnline);
+      socket.userinfo = user;
+      users[socket.userinfo.username] = socket.userinfo;
+      socket.emit('updatechat', 'SERVER', 'you have connected');
+      socket.broadcast.emit('updatechat', 'SERVER', user.username + ' has connected');
+      io.sockets.emit('updateusers', users); 
    })
+
+   socket.on('disconnect', () => {
+      delete users[socket.userinfo.username];
+      io.sockets.emit('updateusers', users);
+      socket.broadcast.emit('updatechat', 'SERVER', socket.userinfo.username + ' has disconnected');
+   });
 });
 
 http.listen(port, () => {
