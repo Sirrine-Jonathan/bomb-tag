7a85ca6f68675e0c111848410b727a8407d36ae6
jonsirrine <sir15004@byui.edu>
2018-07-03 23:27:40 -0700

    Second user becomes it on start

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 const port = (process.env.PORT) ? process.env.PORT:8888;
 let users = {};
+let numOfUsers = 0;
+let someoneIt = false;
 const SERVER_COLOR = "#FFFFFF";
 
 app.use(express.static('public'));
@@ -11,6 +13,13 @@ io.on('connection', (socket) => {
    io.emit('updateusers', users);
 
    socket.on('new user', (user) => {
+      numOfUsers++;
+      console.log("numOfUsers: " + numOfUsers);
+      if (numOfUsers === 2 && someoneIt === false){
+         user.it = true;
+         io.to(user.id).emit('tagged');
+         someoneIt = true;
+      }
       socket.userinfo = user;
       users[socket.userinfo.name] = socket.userinfo;
       socket.emit('updatechat', 'SERVER', SERVER_COLOR, 'you have connected');
@@ -25,9 +34,44 @@ io.on('connection', (socket) => {
    });
 
    socket.on('disconnect', () => {
+
+      // only account for sockets that have joined
       if (socket.userinfo){
-         delete users[socket.userinfo.name];
+
+         // handle changing who is it
+         if (users[socket.userinfo.name] && users[socket.userinfo.name].it) {
+             delete users[socket.userinfo.name];
+             numOfUsers--;
+             console.log("numOfUsers: " + numOfUsers);
+             someoneIt = false;
+             if (numOfUsers > 1) {
+                 // make someone else be it.
+                 for (let i = 0; i < 10; i++){
+                     console.log("test rand: " + Math.floor(Math.random() * numOfUsers));
+                 }
+                 let randomNum = Math.floor(Math.random() * numOfUsers);
+                 console.log("random Number: " + randomNum);
+                 let i = 0;
+                 for (user in users) {
+                     if (i == randomNum) {
+                         users[user].it = true;
+                         io.to(users[user].id).emit('tagged');
+                         console.log(users[user].name + " is it: " + users[user].it);
+                         someoneIt = true;
+                     }
+                     i++;
+                 }
+             }
+         } else {
+             delete users[socket.userinfo.name];
+             numOfUsers--;
+             console.log("numOfUsers: " + numOfUsers);
+         }
+
+         // update clients
          io.sockets.emit('updateusers', users);
+         socket.emit('disconnected');
+         socket.emit('updatechat', 'SERVER', SERVER_COLOR, "You have disconnected");
          socket.broadcast.emit('updatechat', 'SERVER', SERVER_COLOR, socket.userinfo.name + ' has disconnected');
       }
    });
--- public/client.js
+++ public/client.js
@@ -1,7 +1,9 @@
-var usersOnline = {};
-var clientID = '';
+let usersOnline = {};
+let numOfUsers = 0;
+let userName = null; // get from some sort of storage eventually
+
 window.onload = function(){  
-  var socket = io();
+  let socket = io();
 
 
   /*
@@ -9,17 +11,17 @@ window.onload = function(){
   */
   let uInput = document.querySelector("#usernameInput");
   uInput.addEventListener('keypress', (e) => {
-    if (e.charCode == 13){
+    if (e.key === "Enter"){
       let username = uInput.value;
-
+      userName = username;
       // validate unique name constraint
       let unique = true;
       for (let user in usersOnline){
-        if (usersOnline[user].name == username){
+        if (usersOnline[user].name === username){
           unique = false;
           break;
         }
-      };
+      }
       if (!unique){
         let errorMsg = document.querySelector('#usernameError');
         errorMsg.innerHTML = "username taken";
@@ -31,9 +33,9 @@ window.onload = function(){
       }
       
       function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
           color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
@@ -44,13 +46,15 @@ window.onload = function(){
         'name': username,
         'it': false,
         'color': getRandomColor()
-      }
+      };
 
       // empty head
       let head = document.getElementById('header');
       let errorMsg = document.querySelector('#usernameError');
-      head.removeChild(uInput);
-      head.removeChild(errorMsg);
+      //head.removeChild(uInput);
+      //head.removeChild(errorMsg);
+      uInput.style.display = "none";
+      errorMsg.style.display = "none";
 
       // put user display in head
       let uDisplay = document.createElement('span');
@@ -69,12 +73,12 @@ window.onload = function(){
   */
   let chatInput = document.querySelector('#chatInput');
   chatInput.addEventListener('keypress', (e) => {
-    if (e.charCode == 13){
+    if (e.key === "Enter"){
       socket.emit('sendchat', chatInput.value);
       chatInput.innerHTML = '';
       chatInput.value = '';
     }
-  })
+  });
 
   /*
     update chat
@@ -82,7 +86,6 @@ window.onload = function(){
   socket.on('updatechat', (username, color, msg) => {
     let newChat = document.createElement('span');
     newChat.innerHTML = "<b style='color:" + color + "'>" + username + ":</b> " + msg + '<br />';
-    console.log(newChat.innerHTML);
     let chat = document.querySelector('#chat');
     chat.appendChild(newChat);
   });
@@ -94,10 +97,31 @@ window.onload = function(){
   */
   socket.on('updateusers', (users) => {
     usersOnline = users;
-    console.log(usersOnline);
     updateUsers();
   });
 
+  /*
+    client disconnected
+  */
+  socket.on('disconnected', () => {
+      // reapply inputs
+      uInput.style.display = "block";
+      errorMsg.style.display = "block";
+
+      //remove uDisplay
+      let head = document.getElementById('header');
+      let uDisplay = document.querySelector('#uDisplay');
+      head.removeChild(uDisplay);
+  });
+
+  /*
+    client has been tagged
+  */
+  socket.on('tagged', () => {
+      console.log("You are it!");
+      let uDisplay = document.querySelector("#uDisplay");
+      uDisplay.innerHTML = userName + "<span style='color:red'> [!]</span>";
+  });
 
   /*
     updates the UI
@@ -107,9 +131,11 @@ window.onload = function(){
     uo.innerHTML = '';
     for (user in usersOnline){
       let li = document.createElement('li');
-	    li.style.borderRight = "50px solid " + usersOnline[user].color;
-      li.innerHTML = usersOnline[user].name + ((usersOnline[user].it) ? "<span style='color: red'> <!></span>":"");
+      li.style.borderRight = "50px solid " + usersOnline[user].color;
+      li.innerHTML = usersOnline[user].name;
+      if (usersOnline[user].it)
+          li.innerHTML += "<span style='color:red'> [!]</span>";
       uo.appendChild(li);
-    };
+    }
   }
-}
+};
