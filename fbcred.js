a5bf7e372e1d61634f4d19d4c211ae5e01ce3c63
jonsirrine <sir15004@byui.edu>
2018-07-12 11:35:03 -0700

    changed dumb mistake

--- fbcred.js
+++ fbcred.js
@@ -1,5 +1,5 @@
 module.exports = {
     "app_id": process.env.APP_ID || "2072017049705163",
     "app_secret": process.env.APP_SECRET || "75cb23048038cd384a32cc56d7648499",
-    "callback": process.env.APP_SECRET || "https://localhost:8888/fblogin/return"
+    "callback": process.env.CALLBACK || "https://localhost:8888/fblogin/return"
 };
